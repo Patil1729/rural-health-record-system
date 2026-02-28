950f929d0142343b1bcb116d1a5ce924e16c9791
Patil1729 <patilshubhamn1729@gmail.com>
2026-02-28 23:54:43 +0530

    Initial Project Setup

---
+++ src/test/java/com/ruralHealth/RuralHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruralHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
